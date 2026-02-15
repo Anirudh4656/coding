1b2ef44b4b27bea2556c29db5d0ee9834da7538a
naveen <gameog8@gmail.com>
2026-02-15 09:39:00 +0530

    week-2

--- hashset-map.cpp/intersectionOfTwoArrays.cpp
+++ hashset-map.cpp/intersectionOfTwoArrays.cpp
@@ -52,3 +52,26 @@ i++;
         
     }
 }; 
+//what if duplicates are allowed
+//can use two pointer firstly we have to sort overall time complexity will be O(nlogn+ m logm)
+//
+// What if nums1's size is small compared to nums2's size? Which algorithm is better?
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+         vector<int>ans;
+        int n1=nums1.size();
+          int n2=nums2.size();
+          if(n1<n2)return intersect(nums2,nums1);
+unordered_map<int,int>mpp;
+
+   for(int i=0;i<n2;i++){
+    mpp[nums2[i]]++;
+   }
+   for(int i:nums1){
+    if(mpp[i]>0){
+        ans.push_back(i);
+        mpp[i]--;
+    }
+   }
+   return ans;
+    }
+};
